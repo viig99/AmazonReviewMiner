72510fe5453b85d319756c3d56c2a3cbbbce5009
viig99 <accio.arjun@gmail.com>
2023-10-13 21:28:52 -0400

    All necessary changes.

---
+++ include/AdvancedMath.cppm
@@ -0,0 +1,14 @@
+//
+// Created by vigi99 on 10/11/23.
+//
+
+export module AdvancedMath;
+
+namespace AdvancedMath {
+    export int add(int a, int b) {
+        return a + b;
+    }
+    export int sub(int a, int b) {
+        return a - b;
+    }
+}
---
+++ main.cpp
@@ -0,0 +1,10 @@
+import AdvancedMath = math;
+#include <iostream>
+#include "spdlog/spdlog.h"
+
+int main() {
+    spdlog::info("Hello, World!");
+    spdlog::info("5 + 3 = {}", AdvancedMath::add(5, 3));
+    spdlog::info("5 - 3 = {}", AdvancedMath::sub(5, 3));
+    return 0;
+}
